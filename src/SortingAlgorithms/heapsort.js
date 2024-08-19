be261ef6bc8306b44001bd50998769651b34e0fd
Hevial <onicar04@gmail.com>
2024-08-19 14:53:23 +0200

    feat: add css animations and convert bar height from px to %

--- src/SortingAlgorithms/heapsort.js
+++ src/SortingAlgorithms/heapsort.js
@@ -1,105 +1,114 @@
 export function getHeapSortAnimations(array) {
-    const animations = [];
-    if (array.length <= 1) return array;
-    heapSort(array, animations);
-    return animations;
+  const animations = [];
+  if (array.length <= 1) return array;
+  heapSort(array, animations);
+  return animations;
 }
 
 class MaxHeap {
-    constructor() {
-        this.heap = [];
+  constructor() {
+    this.heap = [];
+  }
+
+  parentIndex(index) {
+    return Math.floor((index - 1) / 2);
+  }
+
+  leftChildIndex(index) {
+    return 2 * index + 1;
+  }
+
+  rightChildIndex(index) {
+    return 2 * index + 2;
+  }
+
+  swap(a, b) {
+    let temp = this.heap[a];
+    this.heap[a] = this.heap[b];
+    this.heap[b] = temp;
+  }
+
+  insert(item, animations) {
+    this.heap.push(item);
+    let index = this.heap.length - 1;
+    let parent = this.parentIndex(index);
+
+    while (this.heap[parent] && this.heap[parent] < this.heap[index]) {
+      animations.push(
+        { barsIdx: [parent, index], action: 'compare' },
+        {
+          barsIdx: [parent, index],
+          barHeight: [this.heap[parent], this.heap[index]],
+          action: 'swap'
+        },
+        { barsIdx: [parent, index], action: 'reset' }
+      );
+
+      this.swap(parent, index);
+      //swap(arr, parent, index);
+      index = this.parentIndex(index);
+      parent = this.parentIndex(index);
+    }
+  }
+
+  delete(animations, i, j) {
+    let item = this.heap.shift(); // the root node gets removed
+    this.heap.unshift(this.heap.pop()); // the last node take its place
+    let index = 0;
+    let leftChild = this.leftChildIndex(index);
+    let rightChild = this.rightChildIndex(index);
+
+    animations.push(
+      { barsIdx: [i, j], action: 'compare' },
+      { barsIdx: [i, j], barHeight: [item, this.heap[j]], action: 'swap' },
+      { barsIdx: [i, j], action: 'reset' }
+    );
+
+    //this leaves us with a completely distorted heap.
+    //So we need to bubble this misplaced value down until the heap property is back in place:
+    while (
+      (this.heap[leftChild] && this.heap[leftChild] > this.heap[index]) ||
+      this.heap[rightChild] > this.heap[index]
+    ) {
+      let max = leftChild;
+      if (this.heap[rightChild] && this.heap[rightChild] > this.heap[max]) {
+        max = rightChild;
+      }
+
+      animations.push(
+        { barsIdx: [max, index], action: 'compare' },
+        {
+          barsIdx: [max, index],
+          barHeight: [this.heap[max], this.heap[index]],
+          action: 'swap'
+        },
+        { barsIdx: [max, index], action: 'reset' }
+      );
+
+      this.swap(max, index);
+      //swap(arr, max, index);
+      index = max;
+      leftChild = this.leftChildIndex(max);
+      rightChild = this.rightChildIndex(max);
     }
 
-    parentIndex(index) {
-        return Math.floor((index - 1) / 2);
-    }
-
-    leftChildIndex(index) {
-        return (2 * index + 1);
-    }
-
-    rightChildIndex(index) {
-        return (2 * index + 2);
-    }
-
-    swap(a, b) {
-        let temp = this.heap[a];
-        this.heap[a] = this.heap[b];
-        this.heap[b] = temp;
-    }
-
-    insert(item, animations) {
-        this.heap.push(item);
-        let index = this.heap.length - 1;
-        let parent = this.parentIndex(index);
-
-        while (this.heap[parent] && this.heap[parent] < this.heap[index]) {
-
-            animations.push(
-                { barsIdx: [parent, index], action: "compare" },
-                { barsIdx: [parent, index], barHeight: [this.heap[parent], this.heap[index]], action: "swap" },
-                { barsIdx: [parent, index], action: "reset" }
-            );
-
-            this.swap(parent, index);
-            //swap(arr, parent, index);
-            index = this.parentIndex(index);
-            parent = this.parentIndex(index);
-        }
-    }
-
-    delete(animations, i, j) {
-        let item = this.heap.shift(); // the root node gets removed
-        this.heap.unshift(this.heap.pop()); // the last node take its place
-        let index = 0;
-        let leftChild = this.leftChildIndex(index);
-        let rightChild = this.rightChildIndex(index);
-
-        animations.push(
-            { barsIdx: [i, j], action: "compare" },
-            { barsIdx: [i, j], barHeight: [item, this.heap[j]], action: "swap" },
-            { barsIdx: [i, j], action: "reset" },
-        )
-
-        //this leaves us with a completely distorted heap.
-        //So we need to bubble this misplaced value down until the heap property is back in place:
-        while (this.heap[leftChild] && this.heap[leftChild] > this.heap[index] || this.heap[rightChild] > this.heap[index]) {
-            let max = leftChild;
-            if (this.heap[rightChild] && this.heap[rightChild] > this.heap[max]) {
-                max = rightChild
-            }
-
-            animations.push(
-                { barsIdx: [max, index], action: "compare" },
-                { barsIdx: [max, index], barHeight: [this.heap[max], this.heap[index]], action: "swap" },
-                { barsIdx: [max, index], action: "reset" },
-            )
-
-            this.swap(max, index);
-            //swap(arr, max, index);
-            index = max;
-            leftChild = this.leftChildIndex(max);
-            rightChild = this.rightChildIndex(max);
-        }
-
-        return item;
-    }
+    return item;
+  }
 }
 
 function heapSort(arr, animations) {
-    let sorted = [];
-    let heap1 = new MaxHeap();
+  let sorted = [];
+  let heap1 = new MaxHeap();
 
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = heap1.insert(arr[i], animations);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = heap1.insert(arr[i], animations);
+  }
 
-    }
+  //console.log("Heap: ", heap1.heap);
 
-    //console.log("Heap: ", heap1.heap);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = sorted.unshift(heap1.delete(animations, i, arr.length - i - 1));
+  }
 
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = sorted.unshift(heap1.delete(animations, i, arr.length - i - 1));
-    }
-
-    //return sorted;
+  //return sorted;
 }
--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -14,224 +14,229 @@ const PRIMARY_COLOR = 'blue';
 // This is the color of array bars that are being compared throughout the animations.
 const SECONDARY_COLOR = 'red';
 
-let isSorted = false
-let reset = false
+let isSorted = false;
+let reset = false;
 
 export default class SortingVisualizer extends React.Component {
-    constructor(props) {
-        super(props)
-        this.timeouts = []
-        this.state = { array: [], speed: 4 };
+  constructor(props) {
+    super(props);
+    this.timeouts = [];
+    this.state = { array: [], speed: 4 };
+  }
+
+  componentDidMount() {
+    this.resetArray();
+  }
+
+  disableButtons() {
+    const btns = document.getElementsByClassName('button');
+    const slider = document.querySelector('.speed-slider');
+    slider.classList.add('disabled');
+    for (let i = 1; i < btns.length; i++) {
+      btns[i].classList.add('disabled');
+    }
+  }
+
+  enableButtons() {
+    const btns = document.getElementsByClassName('button');
+    const slider = document.querySelector('.speed-slider');
+    slider.classList.remove('disabled');
+    for (let i = 1; i < btns.length; i++) {
+      btns[i].classList.remove('disabled');
+    }
+  }
+
+  resetArray() {
+    this.enableButtons();
+
+    isSorted = false;
+
+    this.timeouts.forEach(timeout => clearTimeout(timeout));
+    this.timeouts = [];
+
+    const array = [];
+    const maxWidth = window.innerWidth * 0.32 * (2 / 4); // Il moltiplicatore è basato sulla larghezza della barra
+
+    for (let i = 0; i < maxWidth; i++) {
+      array.push(Math.min(Math.random(), 0.99));
     }
 
-    componentDidMount() {
-        this.resetArray();
+    this.setState({ array }, () => {
+      // Questa funzione di callback viene eseguita dopo che il componente è stato aggiornato
+      const arrayBars = document.getElementsByClassName('array-bar');
+      for (let i = 0; i < arrayBars.length; i++) {
+        const heightValue = this.state.array[i] * 100;
+        arrayBars[i].style.height = `${heightValue}%`;
+        arrayBars[i].style.backgroundColor = PRIMARY_COLOR;
+      }
+    });
+  }
+
+  mergeSort() {
+    isSorted = true;
+    console.log('Array originale:', this.state.array);
+    const animations = getMergeSortAnimations(this.state.array);
+    console.log('Array originale sortato:', this.state.array);
+    this.animate(animations);
+  }
+
+  quickSort() {
+    isSorted = true;
+    console.log('Array originale:', this.state.array);
+    const animations = getQuickSortAnimations(this.state.array);
+    console.log('Array originale sortato:', this.state.array);
+    this.animate(animations);
+  }
+
+  heapSort() {
+    isSorted = true;
+    console.log('Array originale:', this.state.array);
+    const animations = getHeapSortAnimations(this.state.array);
+    console.log('Array originale sortato:', this.state.array);
+    this.animate(animations);
+  }
+
+  bubbleSort() {
+    isSorted = true;
+    console.log('Array originale:', this.state.array);
+    const animations = getBubbleSortAnimations(this.state.array);
+    console.log('Array originale sortato:', this.state.array);
+    this.animate(animations);
+  }
+
+  animate(animations) {
+    this.disableButtons();
+    const speed = this.state.speed;
+    const arrayBars = document.getElementsByClassName('array-bar');
+
+    //console.log(animations);
+    for (let i = 0; i < animations.length; i++) {
+      const animation = animations[i];
+      const [barOneIdx, barTwoIdx] = animation.barsIdx;
+
+      const barOneStyle = arrayBars[barOneIdx].style;
+      const barTwoStyle = arrayBars[barTwoIdx].style;
+
+      // comparing the two values
+      switch (animation.action) {
+        case 'compare':
+          //comparing the two values
+          const compareTimeout = setTimeout(() => {
+            barOneStyle.backgroundColor = SECONDARY_COLOR;
+            barTwoStyle.backgroundColor = SECONDARY_COLOR;
+          }, (i * ANIMATION_SPEED_MS) / speed);
+
+          this.timeouts.push(compareTimeout);
+          break;
+        case 'swap':
+          //swapping the two values
+          const swapTimeout = setTimeout(() => {
+            const [barOneHeight, barTwoHeight] = animation.barHeight;
+
+            barOneStyle.height = `${barTwoHeight * 100}%`;
+            if (barOneHeight) barTwoStyle.height = `${barOneHeight * 100}%`;
+          }, (i * ANIMATION_SPEED_MS) / speed);
+
+          this.timeouts.push(swapTimeout);
+          break;
+
+        case 'reset':
+          // after comparing and swapping or not, reset back to the primary color
+          const resetTimeout = setTimeout(() => {
+            barOneStyle.backgroundColor = PRIMARY_COLOR;
+            barTwoStyle.backgroundColor = PRIMARY_COLOR;
+          }, (i * ANIMATION_SPEED_MS) / speed);
+
+          this.timeouts.push(resetTimeout);
+          break;
+
+        default:
+          console.log('Action not possible! Current anim obj: ', animation);
+          break;
+      }
     }
 
-    disableButtons() {
-        const btns = document.getElementsByClassName('button')
-        const slider = document.querySelector('.speed-slider')
-        slider.classList.add('disabled')
-        for (let i = 1; i < btns.length; i++) {
-            btns[i].classList.add('disabled')
-        }
-    }
+    const buttonTimeout = setTimeout(() => {
+      this.enableButtons();
+    }, (animations.length * ANIMATION_SPEED_MS) / speed);
 
-    enableButtons() {
-        const btns = document.getElementsByClassName('button')
-        const slider = document.querySelector('.speed-slider')
-        slider.classList.remove('disabled')
-        for (let i = 1; i < btns.length; i++) {
-            btns[i].classList.remove('disabled')
-        }
-    }
+    animations.length = 0;
+    this.timeouts.push(buttonTimeout);
+  }
 
-    resetArray() {
-        isSorted = false
-        reset = true
+  render() {
+    const { array } = this.state;
 
-        this.timeouts.forEach(timeout => clearTimeout(timeout));
-        this.timeouts = []
+    return (
+      <>
+        <div className='array-container'>
+          {array.map((value, idx) => (
+            <div
+              className='array-bar'
+              key={idx}
+              style={{ height: `${value * 100}%` }}
+            ></div>
+          ))}
+        </div>
+        <div className='tool-bar'>
+          <div>
+            <span id='title'>Sorting Visualizer</span>
+          </div>
+          <div className='menu'>
+            <p className='speed-value'>{this.state.speed}</p>
+            <div className='slider'>
+              <p className='slider-label'>Speed Slider</p>
+              <input
+                className='speed-slider'
+                type='range'
+                value={this.state.speed}
+                min='1'
+                max='9'
+                step='1'
+                onChange={e => {
+                  this.setState({ speed: parseInt(e.target.value) });
+                }}
+              />
+            </div>
 
-        const array = [];
-        const maxHeight = window.innerHeight * 0.91;
-        const maxWidth = window.innerWidth * 0.32 * (2 / 4); //the multiplier is based on the bar width
-
-        for (let i = 0; i < maxWidth; i++) {
-            array.push(randomIntFromInterval(5, maxHeight));
-        }
-
-        this.setState({ array });
-
-        this.enableButtons()
-        const arrayBars = document.getElementsByClassName('array-bar');
-        for (let i = 0; i < arrayBars.length; i++) {
-            arrayBars[i].style.height = `${this.state.array[i]}px`
-            arrayBars[i].style.backgroundColor = PRIMARY_COLOR
-        }
-    }
-
-    mergeSort() {
-        isSorted = true
-        reset = false
-        console.log("Array originale:", this.state.array);
-        const animations = getMergeSortAnimations(this.state.array);
-        console.log("Array originale sortato:", this.state.array);
-        this.animate(animations);
-    }
-
-    quickSort() {
-        isSorted = true
-        reset = false
-        console.log("Array originale:", this.state.array);
-        const animations = getQuickSortAnimations(this.state.array);
-        console.log("Array originale sortato:", this.state.array);
-        this.animate(animations);
-    }
-
-    heapSort() {
-        isSorted = true
-        reset = false
-        console.log("Array originale:", this.state.array);
-        const animations = getHeapSortAnimations(this.state.array);
-        console.log("Array originale sortato:", this.state.array);
-        this.animate(animations);
-    }
-
-    bubbleSort() {
-        isSorted = true
-        reset = false
-        console.log("Array originale:", this.state.array);
-        const animations = getBubbleSortAnimations(this.state.array);
-        console.log("Array originale sortato:", this.state.array);
-        this.animate(animations);
-
-    }
-
-    animate(animations) {
-
-        this.disableButtons()
-        const speed = this.state.speed
-        const arrayBars = document.getElementsByClassName('array-bar');
-
-        console.log(animations)
-        for (let i = 0; i < animations.length; i++) {
-
-            const animation = animations[i]
-            const [barOneIdx, barTwoIdx] = animation.barsIdx
-
-            const barOneStyle = arrayBars[barOneIdx].style
-            const barTwoStyle = arrayBars[barTwoIdx].style
-
-            // comparing the two values
-
-            switch (animation.action) {
-                case "compare":
-
-                    //comparing the two values
-                    const compareTimeout = setTimeout(() => {
-                        if (reset) return
-
-                        barOneStyle.backgroundColor = SECONDARY_COLOR
-                        barTwoStyle.backgroundColor = SECONDARY_COLOR
-                    }, i * ANIMATION_SPEED_MS / speed);
-
-                    this.timeouts.push(compareTimeout)
-                    break
-                case "swap":
-
-                    //swapping the two values
-                    const swapTimeout = setTimeout(() => {
-                        if (reset) return
-
-                        const [barOneHeight, barTwoHeight] = animation.barHeight
-
-                        barOneStyle.height = `${barTwoHeight}px`
-                        if (barOneHeight)
-                            barTwoStyle.height = `${barOneHeight}px`
-                    }, i * ANIMATION_SPEED_MS / speed);
-
-                    this.timeouts.push(swapTimeout)
-                    break
-
-                case "reset":
-
-                    // after comparing and swapping or not, reset back to the primary color
-                    const resetTimeout = setTimeout(() => {
-                        if (reset) return
-
-                        barOneStyle.backgroundColor = PRIMARY_COLOR
-                        barTwoStyle.backgroundColor = PRIMARY_COLOR
-                    }, i * ANIMATION_SPEED_MS / speed);
-
-                    this.timeouts.push(resetTimeout)
-                    break
-
-                default:
-                    console.log("Action not possible! Current anim obj: ", animation);
-                    break
-
-            }
-        }
-
-        const buttonTimeout = setTimeout(() => {
-            this.enableButtons()
-        }, animations.length * ANIMATION_SPEED_MS / speed);
-
-        animations.length = 0
-        this.timeouts.push(buttonTimeout)
-
-    }
-
-    render() {
-        const { array } = this.state;
-
-        return (
-            <>
-                <div className='array-container'>
-                    {array.map((value, idx) => (
-                        <div className='array-bar'
-                            key={idx}
-                            style={{ height: `${value}px` }}></div>
-                    ))}
-
-                </div>
-                <div className='tool-bar' >
-                    <div>
-                        <span id="title">Sorting Visualizer</span>
-                    </div>
-                    <div className='menu'>
-                        <p className="speed-value">{this.state.speed}</p>
-                        <div className='slider'>
-                            <p className='slider-label'>Speed Slider</p>
-                            <input
-                                className="speed-slider"
-                                type="range"
-                                value={this.state.speed}
-                                min="1"
-                                max="9"
-                                step="1"
-                                onChange={(e) => {
-                                    this.setState({ speed: parseInt(e.target.value) })
-                                }}
-                            />
-                        </div>
-
-                        <button className='button' onClick={() => this.resetArray()}>Generate New Array</button>
-                        <button className='button' onClick={() => { if (!isSorted) this.mergeSort() }}>Merge Sort</button>
-                        <button className='button' onClick={() => { if (!isSorted) this.quickSort() }}>Quick Sort</button>
-                        <button className='button' onClick={() => { if (!isSorted) this.heapSort() }}>Heap Sort</button>
-                        <button className='button' onClick={() => { if (!isSorted) this.bubbleSort() }}>Bubble Sort</button>
-                    </div>
-
-                </div>
-            </>
-        );
-    };
-
+            <button className='button' onClick={() => this.resetArray()}>
+              Generate New Array
+            </button>
+            <button
+              className='button'
+              onClick={() => {
+                if (!isSorted) this.mergeSort();
+              }}
+            >
+              Merge Sort
+            </button>
+            <button
+              className='button'
+              onClick={() => {
+                if (!isSorted) this.quickSort();
+              }}
+            >
+              Quick Sort
+            </button>
+            <button
+              className='button'
+              onClick={() => {
+                if (!isSorted) this.heapSort();
+              }}
+            >
+              Heap Sort
+            </button>
+            <button
+              className='button'
+              onClick={() => {
+                if (!isSorted) this.bubbleSort();
+              }}
+            >
+              Bubble Sort
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
-
-function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
